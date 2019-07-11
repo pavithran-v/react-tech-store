e6b3a0e73e18e28eb69d9244e67ec4035bb3350b
pavithran-v <pavithranmech1998@gmail.com>
2019-07-11 08:04:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Tech Store</h1>
+    </div>
+  );
+}
+
+export default App;
